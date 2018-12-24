9c2662df5d4eb13fd94c67d1fa61134e28212601
Zander_M <sherlingford@yahoo.com>
2018-12-24 11:26:19 +0800

    starting project

---
+++ AM.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Asset Management
+Plugin URI: 
+Description: 插件的简单描述
+Version: 插件版本号, 例如: 1.0
+Author: 插件作者
+Author URI: http://URI_Of_The_Plugin_Author作者地址
+*/
+?>
---
+++ readme.txt
@@ -0,0 +1,1 @@
+okfdpasj jfieowp
